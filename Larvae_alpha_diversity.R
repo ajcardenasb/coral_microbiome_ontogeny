a20ccd5349ab1a21c060b34cb7106974a45d8e7d
Anny Cárdenas <ajcardenasb1@gmail.com>
2022-05-11 12:46:37 +0200

    Create Larvae_alpha_diversity.R

---
+++ Larvae_alpha_diversity.R
@@ -0,0 +1,102 @@
+library(vegan)
+library(ggplot2)
+library(GUniFrac)
+library(patchwork)
+library(emmeans)
+library(lme4)
+
+setwd("~/Documents/Bioinformatics_scripts/R_scripts/Valentine_et_al/paper1/")
+
+map=read.table("inputs/Larvae_metadata.txt", header = T, sep = "\t", row.names = 1)
+asv=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,1:23]
+tax=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,24:29]
+tax$Family=gsub("Alcanivoracaceae1", "Alcanivoracaceae", tax$Family)
+
+###rarefying
+cnts=t(asv)
+min(rowSums(cnts)) # determine sample with lowest counts
+asv.rar=Rarefy(cnts, min(rowSums(cnts)))$otu.tab.rff
+
+############################################################
+##################### Alpha-diversity ######################
+############################################################
+
+alpha=as.data.frame(t(estimateR(asv.rar)))
+alpha$Shannon=diversity(asv.rar, index = "shannon")
+alpha$Stage=map$stage[match(rownames(alpha), rownames(map))]
+
+##################################################
+##################### Stats ######################
+##################################################
+#Shannon
+sha_model=lm(Shannon~ Stage, data = alpha)
+anova(sha_model) 
+summary(sha_model) 
+
+#pairwise 
+sha_pairs = emmeans(sha_model, pairwise ~ Stage, weights = "proportional", adjust="none")
+rbind(sha_pairs$contrasts, adjust="fdr")
+sha_pairs_df=as.data.frame(sha_pairs[["contrasts"]])
+write.table(sha_pairs_df, "outputs/anova_shannon_pairwise.txt", sep = "\t", row.names = T, quote = F)
+
+
+#Chao1
+cha_model=lm(S.chao1~ Stage, data = alpha)
+anova(cha_model) 
+summary(cha_model) 
+
+#pairwise 
+cha_pairs = emmeans(cha_model, pairwise ~ Stage, weights = "proportional", adjust="none")
+rbind(cha_pairs$contrasts, adjust="fdr")
+cha_pairs_df=as.data.frame(cha_pairs[["contrasts"]])
+write.table(cha_pairs_df, "outputs/anova_chao1_pairwise.txt", sep = "\t", row.names = T, quote = F)
+
+
+## QC models
+
+res_sha <- resid(sha_model)
+plot(fitted(sha_model), res_sha)
+qqnorm(res_sha)
+qqline(res_sha) 
+plot(density(res_sha))
+
+res_cha <- resid(cha_model)
+plot(fitted(cha_model), res_cha)
+qqnorm(res_cha)
+qqline(res_cha) 
+plot(density(res_cha))
+
+
+# boxplots
+alpha$Stage=factor(alpha$Stage, levels = c("Egg","Sperm","Larvae15d" ,"Larvae17d", "Larvae28d","Seawater"))
+
+shan=ggplot(alpha, aes(x=Stage, y=Shannon, fill=Stage)) + 
+  stat_boxplot(geom = "errorbar")  + 
+  geom_boxplot(alpha = 1) +  
+  scale_fill_manual(values=c("#FF928B","#B9C0DA", "#645617", "#B79D2A","#E1CE7A", "#2589BD"))  + 
+  theme_bw() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
+  labs( y= "Shannon diversity", x="")  + 
+  annotate(geom="text", x=1, y=0.5, label= "A") + 
+  annotate(geom="text", x=2, y=0.5, label= "A") + 
+  annotate(geom="text", x=3, y=0.5, label= "A") + 
+  annotate(geom="text", x=4, y=0.5, label= "B") + 
+  annotate(geom="text", x=5, y=0.5, label= "C") + 
+  annotate(geom="text", x=6, y=0.5, label= "D") 
+
+cha1=ggplot(alpha, aes(x=Stage, y=S.chao1, fill=Stage)) + 
+  stat_boxplot(geom = "errorbar")  + 
+  geom_boxplot(alpha = 1) +  
+  scale_fill_manual(values=c("#FF928B","#B9C0DA", "#645617", "#B79D2A","#E1CE7A", "#2589BD"))  + 
+  theme_bw() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + 
+  labs( y= "Chao1 estimated richness", x="")  +
+  annotate(geom="text", x=1, y=0.5, label= "A") + 
+  annotate(geom="text", x=2, y=0.5, label= "B") + 
+  annotate(geom="text", x=3, y=0.5, label= "C") + 
+  annotate(geom="text", x=4, y=0.5, label= "A") + 
+  annotate(geom="text", x=5, y=0.5, label= "AD") + 
+  annotate(geom="text", x=6, y=0.5, label= "D") 
+
+pdf("./outputs/Larvae_AlphaDiversiy.pdf", width=5,height=7, pointsize = 12)
+shan/cha1
+dev.off()
+
