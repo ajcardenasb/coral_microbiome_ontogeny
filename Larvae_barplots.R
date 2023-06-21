bec915e5e49128015150612b04df02b7ec8efed9
Anny Cárdenas <ajcardenasb1@gmail.com>
2023-06-21 11:29:41 -0400

    Update Larvae_barplots.R

--- Larvae_barplots.R
+++ Larvae_barplots.R
@@ -1,13 +1,15 @@
 library(reshape2)
 library(ggplot2)
 library(scales)
-
+library(Polychrome)
 setwd("~/Documents/Bioinformatics_scripts/R_scripts/Valentine_et_al/paper1/")
 
 map=read.table("inputs/Larvae_metadata.txt", header = T, sep = "\t", row.names = 1)
-asv=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,1:23]
-tax=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,24:29]
+asv=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,1:17]
+tax=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,19:24]
 tax$Family=gsub("Alcanivoracaceae1", "Alcanivoracaceae", tax$Family)
+map$stage=gsub("Larvae15d", "Larvae T0", map$stage)
+map$stage=gsub("Larvae28d",  "Larvae T1", map$stage)
 
 ## identify top 20 families 
 names(asv)
@@ -22,21 +24,37 @@ colnames(all.l)=c("Taxa","Sample","Abundance")
 all.l$Stage=map$stage[match(all.l$Sample, rownames(map))]
 
 final=all.l %>% group_by(Stage, Sample,Taxa) %>% dplyr::summarise(Abundance=sum(Abundance))
-final$Stage=factor(final$Stage, levels = c("Egg","Sperm","Larvae15d" ,"Larvae17d", "Larvae28d","Seawater"))
+final$Stage=factor(final$Stage, levels = c("Egg","Sperm","Larvae T0" , "Larvae T1","Seawater"))
 
 ## Plot
-P21=c("#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD", "#117777", "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", "#DDAA77", "#771122", "#AA4455", "#C0C0C0")
-#P10=c("#1B9E77" ,"#D95F02" ,"#7570B3" ,"#E7298A", "#66A61E", "#E6AB02" ,"#780116","#A6761D", "#2364aa", "#3da5d9", "#ababab")
+#old_palette=c("#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD", "#117777", "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", "#DDAA77", "#771122", "#AA4455", "#C0C0C0")
+P21 = createPalette(21,  c("#B33C86", "#197BBD", "#C46DF7"))
 
 #pdf("./outputs/Larvae_barplots_families.pdf",  width = 10.5, height =4, pointsize = 12) 
-pdf("./outputs/Larvae_barplots_genus.pdf",  width = 10.5, height =4, pointsize = 12) 
+pdf("./outputs/Larvae_barplots_genus.pdf",  width = 8.5, height =4, pointsize = 12) 
 
 ggplot() + geom_bar(aes(y = Abundance, x = Sample, fill = Taxa), 
                    data = final, stat="identity", position = "fill")  +
   labs( y= "Percentage of 16S rRNA sequences", x="") + 
-  scale_fill_manual("Genus",values=P21)  +   theme_classic() + 
+   theme_classic() + 
   theme( legend.key.size = unit(0.4, "cm"),legend.key.width = unit(0.4,"cm"), legend.text = element_text(size=8),
          legend.position = 'bottom', axis.text.x=element_text(angle=90,hjust=1),) + 
-  guides(fill=guide_legend(nrow=3)) + facet_wrap(~Stage, scales = "free", ncol = 6) + scale_y_continuous( expand = c(0, 0)) 
+  guides(fill=guide_legend(nrow=3)) + facet_wrap(~Stage, scales = "free", ncol = 6) + scale_y_continuous( expand = c(0, 0)) +
+  scale_fill_manual( values=old_palette)
 
 dev.off() 
+
+
+
+#exporting relative abundances at genus and family level
+gen_lev=aggregate(asv, by = list(tax[, 6]), FUN =  sum)
+rownames(gen_lev)=gen_lev$Group.1
+gen_lev=gen_lev[,-1]
+gen.rel=sweep(gen_lev,2,colSums(gen_lev),"/")
+write.table(gen.rel, "outputs/Genus_relAbun.txt", sep = "\t", quote = F, row.names = T)
+
+fam_lev=aggregate(asv, by = list(tax[, 5]), FUN =  sum)
+rownames(fam_lev)=fam_lev$Group.1
+fam_lev=fam_lev[,-1]
+fam.rel=sweep(fam_lev,2,colSums(fam_lev),"/")
+write.table(fam.rel, "outputs/Family_relAbun.txt", sep = "\t", quote = F, row.names = T)
