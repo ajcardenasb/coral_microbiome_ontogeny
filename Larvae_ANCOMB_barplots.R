664eda388ebcd79c2fde3bbc5232cd0f2a6a3a00
Anny Cárdenas <ajcardenasb1@gmail.com>
2024-05-15 13:13:03 -0400

    Update Larvae_ANCOMB_barplots.R

--- Larvae_ANCOMB_barplots.R
+++ Larvae_ANCOMB_barplots.R
@@ -1,51 +1,48 @@
 library(reshape2)
 library(ggplot2)
+library(patchwork)
 
 setwd("~/Documents/Bioinformatics_scripts/R_scripts/Valentine_et_al/paper1/")
 
-anc=read.table( "outputs/Larvae_ANCOM_Results3_noLarvae.txt", sep = "\t", header = T)
-ind=read.table( "outputs/Larvae_indic_species.txt", sep = " ", header = T)
-#comp=subset(anc, (Comparison == "Egg vs Larvae" & Diff_more_abundant == "Egg") | 
- #             (Comparison == "Larvae vs Sperm" & Diff_more_abundant == "Sperm") |
-  #            (Comparison == "Larvae vs Seawater" & Diff_more_abundant == "Seawater"))
-comp=subset(anc, !Diff_more_abundant == "Others")
+anc=read.table( "outputs/Larvae_ANCOM_Results3_May2024.txt", sep = "\t", header = T)
 
-comp$abs_W=abs(comp$W)
-comp_l =comp %>%
-  group_by(Comparison) %>%
-  slice_max(order_by = abs_W, n = 20) %>%
-  arrange(Comparison,abs_W)
-comp_l$label = comp_l$Genus
+anc$abs_LFC=abs(anc$LFG)
+comp_l =anc %>%
+  group_by(Diff_more_abundant) %>% arrange(Diff_more_abundant,abs_LFC)
+
 
 #P6=c("#FF928B","#B9C0DA", "#645617", "#B79D2A","#E1CE7A", "#2589BD")
 
-egg=ggplot(subset(comp_l, Diff_more_abundant =="Egg"), aes(x=reorder(label,abs_W),y=abs_W, label = label)) + 
-  geom_text(aes(y = abs_W), vjust=0, size =3, hjust=0) + geom_bar(stat = "identity", 
-                                                                  aes(fill = Diff_more_abundant)) + 
+egg=ggplot(subset(comp_l, Diff_more_abundant =="Egg"), aes(x=reorder(Taxon,abs_LFC),y=abs_LFC, label = Taxon) ) + 
+  geom_text(aes(y = abs_LFC), vjust=0, size =2, hjust=0) + geom_bar(stat = "identity", aes(fill = Diff_more_abundant)) + 
   coord_flip()  + scale_x_discrete(breaks = NULL) + 
-  theme_bw() + scale_fill_manual("",values =c("#FF928B")) + 
-  ylim(0,25) + labs(y="Effect size", x="Differentially enriched genera",title = "") + theme_bw() + 
-  theme(legend.position = 'bottom') 
+  theme_bw() + scale_fill_manual("",values =c("#2EC4B6")) + 
+  ylim(0,10) + labs(y="Log Fold Change", x="Egg",title = "") + theme_bw() + 
+  theme(legend.position = 'none') 
 
-spe=ggplot(subset(comp_l, Diff_more_abundant =="Sperm"), aes(x=reorder(label,abs_W),y=abs_W, label = label)) + 
-  geom_text(aes(y = abs_W), vjust=0, size =3, hjust=0) + geom_bar(stat = "identity", 
-                                                                  aes(fill = Diff_more_abundant)) + 
+spe=ggplot(subset(comp_l, Diff_more_abundant =="Sperm"), aes(x=reorder(Taxon,abs_LFC),y=abs_LFC, label = Taxon) ) + 
+  geom_text(aes(y = abs_LFC), vjust=0, size =2, hjust=0) + geom_bar(stat = "identity", aes(fill = Diff_more_abundant)) + 
   coord_flip()  + scale_x_discrete(breaks = NULL) + 
-  theme_bw() + scale_fill_manual("",values =c("#B9C0DA")) + 
-  ylim(0,25) + labs(y="Effect size", x="Differentially enriched genera",title = "") + theme_bw() + 
-  theme(legend.position = 'bottom') 
+  theme_bw() + scale_fill_manual("",values =c("#F48C06")) + 
+  ylim(0,10) + labs(y="Log Fold Change", x="Sperm",title = "") + theme_bw() + 
+  theme(legend.position = 'none') 
 
-sea=ggplot(subset(comp_l, Diff_more_abundant =="Seawater"), aes(x=reorder(label,abs_W),y=abs_W, label = label)) + 
-  geom_text(aes(y = abs_W), vjust=0, size =3, hjust=0) + geom_bar(stat = "identity", 
-                                                                  aes(fill = Diff_more_abundant)) + 
+sea=ggplot(subset(comp_l, Diff_more_abundant =="Seawater"), aes(x=reorder(Taxon,abs_LFC),y=abs_LFC, label = Taxon) ) + 
+  geom_text(aes(y = abs_LFC), vjust=0, size =2, hjust=0) + geom_bar(stat = "identity", aes(fill = Diff_more_abundant)) + 
   coord_flip()  + scale_x_discrete(breaks = NULL) + 
-  theme_bw() + scale_fill_manual("",values =c("#2589BD")) + 
-  ylim(0,25) + labs(y="Effect size", x="Differentially enriched genera", title = "") + theme_bw() + 
-  theme(legend.position = 'bottom') 
+  theme_bw() + scale_fill_manual("",values =c("#FFD000")) + 
+  ylim(0,10) + labs(y="Log Fold Change", x="Seawater",title = "") + theme_bw() + 
+  theme(legend.position = 'none') 
 
-pdf("outputs/Larvae_biomarkers.pdf", width = 7, height = 4, pointsize = 6)
-egg+spe+sea
+pdf("outputs/Larvae_biomarkers_updated_May2024.pdf", width = 3, height = 8, pointsize = 6)
+egg/spe/sea
 dev.off()
 
-
-
+pdf("outputs/Larvae_biomarkers_updated_May2024_V2.pdf", width = 4, height = 6, pointsize = 6)
+ggplot(comp_l, aes(x=reorder(Taxon,abs_LFC),y=abs_LFC, label = Taxon) ) + 
+  geom_text(aes(y = abs_LFC), vjust=0, size =3, hjust=0) + geom_bar(stat = "identity", aes(fill = Diff_more_abundant)) + 
+  coord_flip()  + scale_x_discrete(breaks = NULL) + 
+  theme_bw() + scale_fill_manual("",values =c("#F48C06","#2EC4B6", "#FFD000")) + 
+  ylim(0,10) + labs(y="Log Fold Change", x="",title = "") + theme_bw() + 
+  theme(legend.position = 'none')  + facet_grid(Diff_more_abundant~., scales = "free", space = "free")
+dev.off()
