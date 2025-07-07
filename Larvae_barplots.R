ba8892aef3236eafe32efdc33bb843479f253f63
Anny Cárdenas <ajcardenasb1@gmail.com>
2025-07-07 15:40:03 -0400

    Update Larvae_barplots.R

--- Larvae_barplots.R
+++ Larvae_barplots.R
@@ -1,15 +1,16 @@
 library(reshape2)
 library(ggplot2)
 library(scales)
-library(Polychrome)
-setwd("~/Documents/Bioinformatics_scripts/R_scripts/Valentine_et_al/paper1/")
+library(dplyr)
+
+setwd("~/Documents/Bioinformatics_scripts/R_scripts/Ontogeny/paper1/")
 
 map=read.table("inputs/Larvae_metadata.txt", header = T, sep = "\t", row.names = 1)
 asv=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,1:17]
 tax=read.table("outputs/Larvae_ASVs_noContanoOut.txt", header = T, sep = "\t", row.names = 1)[,19:24]
 tax$Family=gsub("Alcanivoracaceae1", "Alcanivoracaceae", tax$Family)
-map$stage=gsub("Larvae15d", "Larvae T0", map$stage)
-map$stage=gsub("Larvae28d",  "Larvae T1", map$stage)
+map$stage=gsub("Larvae15d", "Larvae 15 days", map$stage)
+map$stage=gsub("Larvae28d",  "Larvae 28 days", map$stage)
 
 ## identify top 20 families 
 names(asv)
@@ -24,10 +25,10 @@ colnames(all.l)=c("Taxa","Sample","Abundance")
 all.l$Stage=map$stage[match(all.l$Sample, rownames(map))]
 
 final=all.l %>% group_by(Stage, Sample,Taxa) %>% dplyr::summarise(Abundance=sum(Abundance))
-final$Stage=factor(final$Stage, levels = c("Egg","Sperm","Larvae T0" , "Larvae T1","Seawater"))
+final$Stage=factor(final$Stage, levels = c("Egg","Sperm","Larvae 15 days" , "Larvae 28 days","Seawater"))
 
 ## Plot
-#old_palette=c("#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD", "#117777", "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", "#DDAA77", "#771122", "#AA4455", "#C0C0C0")
+old_palette=c("#771155", "#AA4488", "#CC99BB", "#114477", "#4477AA", "#77AADD", "#117777", "#44AAAA", "#77CCCC", "#117744", "#44AA77", "#88CCAA", "#777711", "#AAAA44", "#DDDD77", "#774411", "#AA7744", "#DDAA77", "#771122", "#AA4455", "#C0C0C0")
 P21 = createPalette(21,  c("#B33C86", "#197BBD", "#C46DF7"))
 
 #pdf("./outputs/Larvae_barplots_families.pdf",  width = 10.5, height =4, pointsize = 12) 
@@ -58,3 +59,20 @@ rownames(fam_lev)=fam_lev$Group.1
 fam_lev=fam_lev[,-1]
 fam.rel=sweep(fam_lev,2,colSums(fam_lev),"/")
 write.table(fam.rel, "outputs/Family_relAbun.txt", sep = "\t", quote = F, row.names = T)
+
+
+#Top genus per stage
+colnames(gen.rel)=map$stage[match(colnames(gen.rel), rownames(map))]
+colnames(gen.rel)=gsub(".[0-9]", "", colnames(gen.rel))
+ave_gen=as.data.frame(t(apply(gen.rel, 1, function(x) tapply(x, colnames(gen.rel), mean))))
+se_gen=as.data.frame(t(apply(gen.rel, 1, function(x) tapply(x, colnames(gen.rel), sd))))
+
+
+rownames(arrange(ave_gen, Egg))[1:20]
+
+ave_gen %>% arrange(desc(Egg))
+ave_gen[order(Egg),]
+
+
+top_egg=gen.rel[order(gen.rel$Egg,decreasing = TRUE),][1:20,1]
+top_sperm=gen_agg[order(rowSums(gen_agg[,colnames(gen_agg) %in% rownames(subset(map, stage == "Sperm"))]),decreasing = TRUE),][1:20,1]
